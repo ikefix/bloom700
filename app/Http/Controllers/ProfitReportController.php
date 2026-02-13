dbc1315a708d7f1d734f6130aa64aac5fe04150b
ottohcharleswills <ottohcharleswills@gmail.com>
2026-02-13 07:36:32 +0100

    db

--- app/Http/Controllers/ProfitReportController.php
+++ app/Http/Controllers/ProfitReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\PurchaseItem;
 use App\Models\Expense;
 use App\Models\Shop;
